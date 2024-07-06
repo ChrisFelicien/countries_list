df3acc956d9c5d9dad42981a0ffa82e43d40506d
Chris_felicie <f.ilungakalenga@gmail.com>
2024-07-06 15:13:10 +0200

    fixing the ui issues

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -4,10 +4,17 @@ const CountryCard = ({ country }) => {
   return (
     <div className='w-full md:w-60 shadow-xl '>
       <img src={country?.flags.svg} alt='country flag' />
-      <div className='flex flex-col gap-4 p-2 text-left'>
-        <p>{country?.name.common} </p>
-        <p> {country?.capital} </p>
-        <p>{country?.population} hab. </p>
+      <div className='flex flex-col gap-4 p-2 text-left pt-8'>
+        <div className='flex justify-between'>
+          <p>{country?.name.common} </p>
+          <p className='bg-slate-600 text-slate-900 rounded-full px-2 py-0'>
+            {country?.region}
+          </p>
+        </div>
+        <div className='flex justify-between'>
+          <p>{country?.capital} </p>
+          <p>{country?.population} hab. </p>
+        </div>
         <div className='flex gap-1 flex-wrap'>
           {country?.borders?.map((con) => (
             <Borders key={crypto.randomUUID()} name={con} />
