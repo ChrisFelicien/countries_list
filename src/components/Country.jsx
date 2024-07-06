745404199b0846c76fde221f6224174c5521b4df
Chris_felicie <f.ilungakalenga@gmail.com>
2024-07-06 15:19:58 +0200

    justify button center

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -3,7 +3,7 @@ import CountryList from "./CountryList";
 
 const Country = ({ country }) => {
   return (
-    <main className=' text-center p-20 text-slate-600 text-lg '>
+    <main className='p-0 text-center sm:p-20 text-slate-600 text-lg '>
       <CountryList countriesList={country} />
     </main>
   );
